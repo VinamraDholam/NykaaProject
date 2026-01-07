8500bda209dcab5a8a67711f8441c9d7b8ea74cd
VinamraDholam <vinamradholam@gmail.com>
2026-01-07 13:45:31 +0530

    2nd commit

--- src/main/java/TestCases/TestData.java
+++ src/main/java/TestCases/TestData.java
@@ -29,7 +29,7 @@ public class TestData  extends BasePageClass{
  
 	@BeforeMethod
 	public void setup() {
-	    invokeBrowser("Edge");
+	    invokeBrowser("Chrome");
 	    homePage = OpenApplication();
 
 	    hairPage = new HairPage(driver);
